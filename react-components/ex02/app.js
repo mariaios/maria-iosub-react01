998befb3af79f12a1adc77ea031f15d8a9b1b6ea
Maria Iosub <maria.salajan8@gmail.com>
2021-10-12 18:51:03 +0300

    stuff

--- react-components/ex02/app.js
+++ react-components/ex02/app.js
@@ -1,8 +1,9 @@
 const person = {
   name: 'Dragos',
   surname: 'Iordache',
-  age: 32,
+  age: 34,
   petOwner: false,
+  // html,javascript,css
   skills: [
     'html',
     'javascript',
@@ -31,3 +32,73 @@ const person = {
     },
   ],
 };
+
+console.warn(`
+  Folosind obiectul person si reduce,
+  afiseaza in consola un string care contine
+  skillurile de pe pozitiile pare ale arrayului, separate prin virgula
+`);
+let message = '';
+
+message = person.skills.reduce((message, skill, index) => {
+  if (index % 2 !== 0) {
+    return message;
+  }
+
+  return `${message}${index === 0 ? '' : ','}${skill}`;
+}, '');
+console.log(message);
+
+console.warn(`
+  In mod similar, afiseaza skillurile care NU incep cu j.
+`);
+message = person.skills.reduce((message, skill, index) => {
+  if (skill.startsWith('j')) {
+    return message;
+  }
+
+  return `${message}${index === 0 ? '' : ','}${skill}`;
+}, '');
+console.log(message);
+
+console.warn(`
+  Folosind reduce afiseaza propozitia: "Prietenii mei se numesc xxx yyy, xxx yyy, xxx yyy."
+`);
+message = person.friends.reduce(
+  (message, { name, surname }, index, friends) => {
+    const punctuation = index === friends.length - 1 ? '.' : ', ';
+
+    return `${message}${name} ${surname}${punctuation}`;
+  },
+  'Prietenii mei se numesc ',
+);
+console.log(message);
+
+console.warn(`
+  Folosind reduce, afiseaza numarul total de ani pe
+  care il au persoanele din arrayul friends, doar
+  daca varsta este mai mare sau egala cu 30 de ani.
+`);
+message = person.friends.reduce((sumYears, friend) => {
+  const { age } = friend;
+
+  return age >= 30 ? sumYears + age : sumYears;
+}, 0);
+console.log(message);
+
+console.warn(`doar skillurile care incep cu j`);
+let filteredSkills = person.skills.reduce((filteredSkills, skill) => {
+  if (skill.startsWith('j')) {
+    return filteredSkills;
+  }
+
+  filteredSkills.push(skill);
+
+  return filteredSkills;
+}, []);
+console.log(filteredSkills);
+
+filteredSkills = person.skills.filter((skill) => {
+  return !skill.startsWith('j');
+});
+console.log(filteredSkills);
---
+++ swapi-cinema/src/App.jsx
@@ -0,0 +1,91 @@
+import { Component, Fragment } from 'react';
+import Films from './components/Films';
+import Search from './components/Search';
+
+const baseUrl = 'https://swapi.dev/api/films';
+
+class App extends Component {
+  state = {
+    busy: true,
+    films: [],
+    errorMessage: '',
+  };
+
+  getFilms() {
+    this.setState({
+      busy: true,
+    });
+
+    // promise chaining
+    fetch(baseUrl)
+      .then((response) => {
+        if (response.status === 404) {
+          throw new Error('404');
+        }
+
+        return response.json();
+      })
+      .then(({ results }) => {
+        this.setState({
+          films: results,
+          busy: false,
+        });
+      })
+      .catch((_) => {
+        this.setState({
+          errorMessage: 'An error has occured.',
+          busy: false,
+        });
+      });
+  }
+
+  renderFilms() {
+    return (
+      <>
+        <h2>Available films</h2>
+
+        <Films films={this.state.films}></Films>
+      </>
+    );
+  }
+
+  renderMainScreen() {
+    if (this.state.busy === true) {
+      return <>... loading</>;
+    }
+
+    if (this.state.busy === false && this.state.errorMessage.length > 0) {
+      return <>{this.state.errorMessage}</>;
+    }
+
+    return this.renderFilms();
+  }
+
+  componentDidMount() {
+    this.getFilms();
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <header className="navbar-expand-md navbar-dark fixed-top bg-dark">
+          <nav className="container d-flex justify-content-between">
+            <h1 className="display-6 text-warning">Swapi Cinema</h1>
+
+            <Search
+              onSearchResults={(films) => {
+                this.setState({
+                  films,
+                });
+              }}
+            ></Search>
+          </nav>
+        </header>
+
+        <main className="container mt-5 pt-5">{this.renderMainScreen()}</main>
+      </Fragment>
+    );
+  }
+}
+
+export default App;
